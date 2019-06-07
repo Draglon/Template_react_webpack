25eda2cf71fde2ec27a8b7c1686a2d0fca8de60d
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-06-07 19:05:37 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,19 +32,6 @@ module.exports = {
     'react',
     'prettier',
   ],
-  // "rules": {
-  //   "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-  //   "babel/no-invalid-this": 1,
-  //   "no-undef": 0,
-  //   "react/prop-types": 0,
-  //   "no-case-declarations": 0,
-  //   "react/no-did-update-set-state": 0,
-  //   "class-methods-use-this": 0,
-  //   "jsx-a11y/click-events-have-key-events": 0,
-  //   "react/jsx-filename-extension": 0,
-  //   "import/no-unresolved": 0,
-  //   "react/jsx-uses-vars": [2],
-  // }
   rules: {
     'linebreak-style': 'off', // с Windows работает плохо
     
