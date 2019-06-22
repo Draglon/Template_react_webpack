74f8ecd78d7d858f58bcac8cca4fb3d6e5177950
serg <s.nazarchuck@gmail.com>
2019-06-22 03:43:08 +0300

    fix axios apiClient

--- app/apiClient/__tests__/config.test.js
+++ app/apiClient/__tests__/config.test.js
@@ -1,31 +1,21 @@
 import axios from 'axios';
-import config from '../config';
 import qs from 'qs';
+import config from '../config';
 
 jest.mock('axios', () => ({
-  create: jest.fn(),
+  create: jest.fn().mockImplementation(() => ({
+    interceptors: {
+      request: {
+        use: jest.fn(),
+      },
+    },
+  })),
 }));
-
-// import { AxiosStatic } from 'axios';
-
-// const mock = jest.genMockFromModule<AxiosStatic>('axios');
-
-// let mock = jest.mock('axios', () => ({
-//   create: jest.fn(),
-// }));
-
-// mock.interceptors = mock.interceptors || {};
-// mock.interceptors.response = mock.interceptors.response || {};
-// mock.interceptors.request.use = jest.fn();
-// mock.create = () => {
-//   return mock;
-// };
 
 describe('API config', () => {
   it('should send request - apiConfig', () => {
     expect(axios.create).toHaveBeenCalledWith({
       baseURL: 'https://api.themoviedb.org/3/',
-      timeout: 1000,
     });
   });
 });
--- app/apiClient/config.js
+++ app/apiClient/config.js
@@ -3,21 +3,15 @@ import qs from 'qs';
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
-  timeout: 1000,
 });
 
 // request header
-instance.interceptors.request.use(
-  config => {
-    config.params = {
-      api_key: '373ab70129f36fd1f65b6d7b24ec2641',
-    };
-    config.paramsSerializer = params => qs.stringify(params);
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  },
-);
+instance.interceptors.request.use(config => {
+  config.params = {
+    api_key: '373ab70129f36fd1f65b6d7b24ec2641',
+  };
+  config.paramsSerializer = params => qs.stringify(params);
+  return config;
+});
 
 export default instance;
