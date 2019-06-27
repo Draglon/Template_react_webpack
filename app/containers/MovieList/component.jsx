07a388075b2d68ae8447138baf45902ce280f769
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-06-27 18:49:37 +0300

    fix commit empty movies

--- app/containers/MovieList/component.jsx
+++ app/containers/MovieList/component.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Empty } from 'antd';
 
 import MovieItem from '../MovieItem';
 
 const MovieListComponent = ({ movies }) => (
   <Row className="top-padding" type="flex" gutter={16}>
     <Col className="cards" span={20} offset={2}>
-      {movies.map(item => (
-        <Col
-          key={item.id}
-          xs={{ span: 12 }}
-          sm={{ span: 12 }}
-          md={{ span: 8 }}
-          lg={{ span: 6 }}
-          xl={{ span: 4 }}
-        >
-          <MovieItem item={item} />
-        </Col>
-      ))}
+      {movies.length ? (
+        movies.map(item => (
+          <Col
+            key={item.id}
+            xs={{ span: 12 }}
+            sm={{ span: 12 }}
+            md={{ span: 8 }}
+            lg={{ span: 6 }}
+            xl={{ span: 4 }}
+          >
+            <MovieItem item={item} />
+          </Col>
+        ))
+      ) : (
+        <Empty
+          className="cards__empty"
+          description="No movies found"
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+        />
+      )}
     </Col>
   </Row>
 );
--- app/containers/Pagination/component.jsx
+++ app/containers/Pagination/component.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Row, Col, Pagination } from 'antd';
 
 const PaginationComponent = ({ query, currentPage, getPage, totalPages }) => (
-  <Row type="flex" justify="center" className="top-padding">
-    <Col>
-      <Pagination
-        className="pagination"
-        defaultCurrent={1}
-        current={currentPage}
-        total={totalPages}
-        onChange={page => getPage(query ? { query, page } : { page })}
-      />
-    </Col>
-  </Row>
+  <>
+    {totalPages ? (
+      <Row type="flex" justify="center" className="top-padding">
+        <Col>
+          <Pagination
+            className="pagination"
+            defaultCurrent={1}
+            current={currentPage}
+            total={totalPages}
+            onChange={page => getPage(query ? { query, page } : { page })}
+          />
+        </Col>
+      </Row>
+    ) : null}
+  </>
 );
 
 export default PaginationComponent;
