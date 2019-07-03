25793ae472682a0891a5f865f338cd8548695b80
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-03 19:49:08 +0300

    fix

--- app/containers/Dashboard/__tests__/container.test.js
+++ app/containers/Dashboard/__tests__/container.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import configureStore from 'redux-mock-store';
-import { shallow, mount } from 'enzyme';
 
-import ConnectedDashboardContainer, { DashboardContainer } from '../container';
+import DashboardContainer from '../container';
 
 describe('DashboardContainer', () => {
   const mockStore = configureStore();
@@ -18,42 +17,39 @@ describe('DashboardContainer', () => {
         },
       },
       search: {
-        query: '',
-        page: 1,
-        results: [],
-        totalPages: 20,
+        data: {
+          query: '',
+          page: 1,
+          results: [],
+          totalPages: 20,
+        },
       },
     },
   };
   const props = {
     trendingRequest: mockFetchTrendingRequest,
     searchRequest: mockFetchSearchRequest,
+    trending: {},
+    search: {},
   };
-  // let store;
-  // let container;
-  // let instance;
-  // let loadData;
+  let store;
+  let container;
+  let instance;
+  let loadData;
 
-  beforeAll(() => {
-    // store = mockStore(state);
-    // container = mount(<ConnectedDashboardContainer store={store} {...props} />).dive();
-    // instance = container.instance();
-    // loadData = jest.spyOn(instance, 'loadData');
-    // instance.componentDidMount();
-  });
+  // beforeAll(() => {
+  store = mockStore(state);
+  container = mount(<DashboardContainer store={store} {...props} />).dive();
+  instance = container.instance();
+  console.log(instance);
+  loadData = jest.mock('trendingRequest', () => {});
+  instance.componentDidMount();
+  // console.log(container);
+  // });
 
   it('dispatches the `trendingRequest()`', () => {
-    // expect(loadData).toHaveBeenCalledWith({ page: 1 });
-    // expect(loadData).toHaveBeenCalledTimes(1);
-    // container.unmount();
+    expect(loadData).toHaveBeenCalledWith({ page: 1 });
+    expect(loadData).toHaveBeenCalledTimes(1);
+    container.unmount();
   });
-
-  // it('+++ check action on dispatching ', () => {
-  //   let action;
-  //   store.dispatch(addInputs(500));
-  //   store.dispatch(subtractInputs(100));
-  //   action = store.getActions();
-  //   expect(action[0].type).toBe("ADD_INPUTS");
-  //   expect(action[1].type).toBe("SUBTRACT_INPUTS");
-  // });
 });
