cd72b952e2a9a16a3aef30123844ca9dff46f203
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-03 19:00:00 +0300

    fix

--- app/store/schema/index.js
+++ app/store/schema/index.js
@@ -9,7 +9,15 @@ export const trending = new schema.Entity('trending');
 // genres
 export const genres = new schema.Entity('genres');
 
+// cast
+export const cast = new schema.Entity('cast');
+
+// cast
+export const crew = new schema.Entity('crew');
+
 // movie
 export const schemaMovie = new schema.Entity('movie', {
   genres: [genres],
+  cast: [cast],
+  crew: [crew],
 });
