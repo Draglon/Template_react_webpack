d4937099ab95631c3a641edb8eaef832da286562
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-05 17:23:22 +0300

    fix genger, cast and crew

--- app/store/theMovieDB/data/reducers.js
+++ app/store/theMovieDB/data/reducers.js
@@ -5,16 +5,32 @@ export const initialState = {
   movies: {},
   movie: {},
   genres: {},
+  cast: {},
+  crew: {},
 };
 
 // Data
 export const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case t.ADD_ENTITIES:
+      if (action.entities.movies) {
+        return {
+          ...state,
+          movies: Object.assign(state.movies, action.entities.movies),
+        };
+      }
+      if (action.entities.movie) {
+        return {
+          ...state,
+          movie: Object.assign(state.movie, action.entities.movie),
+          genres: Object.assign(state.genres, action.entities.genres),
+          cast: Object.assign(state.cast, action.entities.cast),
+          crew: Object.assign(state.crew, action.entities.crew),
+        };
+      }
       return {
         ...state,
         ...action.entities,
-        // movies: action.entities,
       };
     default:
       return state;
--- app/store/theMovieDB/movie/selectors.js
+++ app/store/theMovieDB/movie/selectors.js
@@ -1,28 +1,22 @@
-import { denormalize } from 'normalizr';
-import { schemaMovie } from '../../schema';
-
 export const getMovieById = (state, id) => {
   const movie = state.reducers.data.movie;
   return movie && movie[id];
 };
 
 export const getGenresById = (state, id) => {
-  const movie = state.reducers.data.movie;
+  const movie = state.reducers.data.movie[id];
   const genres = state.reducers.data.genres;
-
-  return Object.assign([], denormalize(genres, schemaMovie, movie[id]));
+  return movie && movie.genres.map(item => genres[item]);
 };
 
 export const getCastById = (state, id) => {
-  const movie = state.reducers.data.movie;
+  const movie = state.reducers.data.movie[id];
   const cast = state.reducers.data.cast;
-
-  return Object.assign([], denormalize(cast, schemaMovie, movie[id]));
+  return movie && movie.cast.map(item => cast[item]);
 };
 
 export const getCrewById = (state, id) => {
-  const movie = state.reducers.data.movie;
+  const movie = state.reducers.data.movie[id];
   const crew = state.reducers.data.crew;
-
-  return Object.assign([], denormalize(crew, schemaMovie, movie[id]));
+  return movie && movie.crew.map(item => crew[item]);
 };
