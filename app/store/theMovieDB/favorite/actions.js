23e091cd7780f0e63798557f143a4ac1a54b25f0
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-12 12:42:30 +0300

    tests for favorites

--- app/store/theMovieDB/favorite/actions.js
+++ app/store/theMovieDB/favorite/actions.js
@@ -4,6 +4,6 @@ export const favoriteRequest = payload => ({ type: t.FAVORITE_REQUEST, payload }
 export const favoriteSuccess = payload => ({ type: t.FAVORITE_SUCCESS, payload });
 export const favoriteFailure = payload => ({ type: t.FAVORITE_FAILURE, payload });
 
-export const addToFvoriteRequest = payload => ({ type: t.ADD_TO_FAVORITE_REQUEST, payload });
-export const addToFvoriteSuccess = payload => ({ type: t.ADD_TO_FAVORITE_SUCCESS, payload });
-export const addToFvoriteFailure = payload => ({ type: t.ADD_TO_FAVORITE_FAILURE, payload });
+export const addToFavoriteRequest = payload => ({ type: t.ADD_TO_FAVORITE_REQUEST, payload });
+export const addToFavoriteSuccess = payload => ({ type: t.ADD_TO_FAVORITE_SUCCESS, payload });
+export const addToFavoriteFailure = payload => ({ type: t.ADD_TO_FAVORITE_FAILURE, payload });
