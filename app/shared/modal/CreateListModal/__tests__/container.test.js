8e54afc8d124b2e5c549bb56f5d8f1a4f64dd0dd
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-18 17:34:55 +0300

    fix after rebase

--- app/shared/modal/CreateListModal/__tests__/container.test.js
+++ app/shared/modal/CreateListModal/__tests__/container.test.js
@@ -4,8 +4,6 @@ import { createListRequest } from '../../../../store/theMovieDB/myLists/actions'
 
 import CreateListModalContainer from '../container';
 
-const spy = jest.spyOn(Modal, 'confirm');
-
 describe('<CreateListModalContainer />', () => {
   const store = configureStore()({});
   store.dispatch = jest.fn();
