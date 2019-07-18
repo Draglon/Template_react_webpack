3bd49b801c215e6a7b3b73c023c1671ecc1ff9f9
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-18 18:41:02 +0300

    test - login form (component)

--- app/components/Home/LoginForm/__tests__/component.test.js
+++ app/components/Home/LoginForm/__tests__/component.test.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 import LoginFormComponent from '../component';
 
-describe('<LoginFormComponent /> component', () => {
-  const props = { setAccess: jest.fn() };
-  let component;
+describe('<LoginFormComponent />', () => {
+  const props = {
+    onSubmit: jest.fn(),
+    loginError: '',
+  };
+  const component = shallow(<LoginFormComponent {...props} />);
 
-  // beforeEach(() => {
-  //   component = shallow(<LoginFormComponent {...props} />);
-  // });
+  describe('request is loading', () => {
+    it('should match snapshot', () => {
+      const propsFormik = {
+        isSubmitting: true,
+      };
+      const rendered = component.find(Formik).renderProp('render')(propsFormik);
+      expect(rendered).toMatchSnapshot();
+    });
+  });
 
-  it('Render property', () => {
-    // expect(component).toMatchSnapshot();
+  describe('request was complited', () => {
+    it('should match snapshot', () => {
+      const propsFormik = {
+        isSubmitting: false,
+      };
+      const rendered = component.find(Formik).renderProp('render')(propsFormik);
+      expect(rendered).toMatchSnapshot();
+    });
+  });
+
+  describe('fields have error', () => {
+    beforeEach(() => {
+      const newProps = {
+        ...props,
+        loginError: 'field with error',
+      };
+      component.setProps(newProps);
+    });
+
+    it('should match snapshot', () => {
+      const propsFormik = {
+        isSubmitting: false,
+      };
+      const rendered = component.find(Formik).renderProp('render')(propsFormik);
+      expect(rendered).toMatchSnapshot();
+    });
+  });
+
+  it('should match snapshot', () => {
+    expect(component).toMatchSnapshot();
   });
 });
