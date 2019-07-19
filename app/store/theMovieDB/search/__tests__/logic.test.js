5c91acf073be015527612584743632181b12283f
SerhiiNazarchuk <serhii.nazarchuk@gmail.com>
2019-07-19 17:38:12 +0300

    tests - logic-search

---
+++ app/store/theMovieDB/search/__tests__/logic.test.js
@@ -0,0 +1,81 @@
+import { normalize } from 'normalizr';
+import { movies } from '../../../schema';
+import { httpClientMock } from '../../../../helpers/httpClientMock';
+
+import searchLogic from '../logic';
+import { searchSuccess } from '../actions';
+import { addEntities } from '../../data/actions';
+
+describe('Search - logic', () => {
+  const dispatch = jest.fn();
+  const done = jest.fn();
+
+  describe('SEARCH_REQUEST', () => {
+    const action = {
+      payload: {
+        query: 'search query',
+        page: 2,
+      },
+    };
+
+    describe('Search SUCCESS', () => {
+      const request = {
+        method: 'get',
+        response: {
+          data: {
+            query: '',
+            results: [{ id: 1 }, { id: 2 }],
+            total_results: 45,
+            page: 2,
+          },
+        },
+      };
+      const apiClient = httpClientMock(request);
+
+      searchLogic.process({ apiClient, action }, dispatch, done);
+      const { data } = request.response;
+      const { entities, result } = normalize(data.results, [movies]);
+      const { query, page } = action.payload;
+
+      it('Should return correct URL', () => {
+        expect(apiClient.get).toHaveBeenCalledWith(
+          `search/movie?query=${query}&page=${page}&language=en-US`,
+        );
+      });
+
+      it('dispatches addEntities()', () => {
+        expect(dispatch).toHaveBeenCalledWith(addEntities(entities));
+      });
+
+      it('dispatches searchSuccess()', () => {
+        expect(dispatch).toHaveBeenCalledWith(
+          searchSuccess({
+            ...data,
+            query,
+            page,
+            results: result,
+          }),
+        );
+      });
+
+      it('calls done', () => {
+        expect(done).toHaveBeenCalled();
+      });
+    });
+
+    describe('Search FAILURE', () => {
+      const apiClient = httpClientMock({
+        method: 'get',
+        reject: true,
+      });
+
+      searchLogic.process({ apiClient, action }, dispatch, done);
+
+      it('Should throw an Error', () => {
+        expect(() => {
+          throw new Error();
+        }).toThrow();
+      });
+    });
+  });
+});
